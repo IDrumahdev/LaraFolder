a8ad87c4e4ede03c18391b84b6d7b65066d8e4c5
rumahdevID <rumahpayment@gmail.com>
2022-09-25 20:11:32 +0700

    fixbug

--- src/Commands/AssetFolder.php
+++ src/Commands/AssetFolder.php
@@ -32,8 +32,8 @@ class AssetFolder extends Command
         /**
          * Example Syntax to create Folder
          */
-        if(!File::exists(public_path('assets/reportx'))) {
-            File::makeDirectory(public_path('/public/assetsx/report/mutasiBank/dailyMail'));
+        if(!File::exists(public_path('assets/AssetFolder'))) {
+            File::makeDirectory(public_path('/public/assets/AssetFolder'));
             Log::info('Message : Success Created Folder');
         } else {
             Log::info('Message : Aready Created Folder');
@@ -41,21 +41,16 @@ class AssetFolder extends Command
             exit;
         } 
 
-        $bar = $this->output->createProgressBar(6);
+        $bar = $this->output->createProgressBar(2);
         $bar->start();
-        for ($i=1; $i <= 6; $i++) {
+        for ($i=1; $i <= 2; $i++) {
             sleep(2);
             $bar->advance();
         }
             $bar->finish();
             $this->newLine(2);
-            $this->line('List Created Folder : ');
-            $this->components->twoColumnDetail('1)', '/public/assets/report/mutasiBank/dailyMail');
-            $this->components->twoColumnDetail('2)', '/public/assets/report/mutasiBank/sendMail');
-            $this->components->twoColumnDetail('3)', '/public/assets/report/paymentGateway/dailyMail');
-            $this->components->twoColumnDetail('4)', '/public/assets/report/paymentGateway/sendMail');
-            $this->components->twoColumnDetail('5)', '/public/assets/report/paymentTransfer/dailyMail');
-            $this->components->twoColumnDetail('6)', '/public/assets/report/paymentTransfer/sendMail');
+            $this->line('List Created Folder ');
+            $this->components->twoColumnDetail('1)', '/public/assets/AssetFolder');
             $this->newLine();
             $this->components->info('Successfully created folder report in assets.');
             $this->line('<bg=green;fg=black>..:: Created by ibnudirsan ::..</>');
